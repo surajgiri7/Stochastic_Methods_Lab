df808da7654f20f094aa7996f0b257b455045add
Suraj Giri <surajgiri@Surajs-MBP.fritz.box>
2022-11-09 21:35:10 +0100

    HW7 P3 plotting fixed

--- HW07_2022_11_02/hw7p3.py
+++ HW07_2022_11_02/hw7p3.py
@@ -6,7 +6,7 @@ HW7: Problem 3
 """
 Problem:
 use Black Scholes formula and plot the Call Price C against
-1. Strike Price K
+1. Stock Price S
 2. interest rate r
 3. volatility sigma
 """
@@ -31,22 +31,29 @@ if __name__ == "__main__":
     T = 1
     r = np.arange(0, 1, 0.01)
     sigma = np.arange(0, 1, 0.01)
+    stock = np.arange(0,1, 0.01)
     # calculating the call price
     # C = Black_Scholes_Formula(S_0, K, T, r, sigma)
     # plotting the graph into 3 subplots
     fig, ax = plt.subplots(3, 1)
-    ax[0].plot(K, Black_Scholes_Formula(S_0, K, T, 0.5, 0.5))
-    ax[0].set_title("Call Price against Strike Price")
-    ax[0].set_xlabel("Strike Price")
+    # changing the size of the figure
+    fig.set_size_inches(8, 8)
+
+    # plotting the graph against Stock Price S
+    ax[0].plot(stock, Black_Scholes_Formula(stock, K, T, 0.5, 0.5))
+    ax[0].set_title("Call Price against Stock Price")
+    ax[0].set_xlabel("Stock Price")
     ax[0].set_ylabel("Call Price")
     ax[0].grid()
 
+    # plotting the graph against interest rate r
     ax[1].plot(r, Black_Scholes_Formula(S_0, 70, T, r, 0.5))
     ax[1].set_title("Call Price against Interest Rate")
     ax[1].set_xlabel("Interest Rate")
     ax[1].set_ylabel("Call Price")
     ax[1].grid()
 
+    # plotting the graph against volatility sigma
     ax[2].plot(sigma, Black_Scholes_Formula(S_0, 70, T, 0.5, sigma))
     ax[2].set_title("Call Price against Volatility")
     ax[2].set_xlabel("Volatility")
